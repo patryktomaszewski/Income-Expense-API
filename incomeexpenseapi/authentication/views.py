57212166b8a9770b55e2c46565df8f558896b97a
patryktomaszewski <58734528+patryktomaszewski@users.noreply.github.com>
2021-06-01 23:36:45 +0200

    status

--- incomeexpenseapi/authentication/views.py
+++ incomeexpenseapi/authentication/views.py
@@ -36,12 +36,12 @@ class RegisterView(generics.GenericAPIView):
         current_site = get_current_site(request).domain
         relative_link = reverse("email-verify")
         absurl = 'http://' + relative_link + '?token=' + str(token)
-        email_body = 'Hi ' + user.username + '\n Use link below to verify your email \n' + absurl
+        email_body = 'Hi ' + user.username + '\n Use linkss below to verify your email \n' + absurl
         data = {'email_body': email_body, 'to_email': user.email, 'email_subject': 'Verify Your email'}
 
         Util.send_email(data)
 
-        return Response(user_data, status=status.HTTP_201_CREATED)
+        return Response(user_data, status=status.HTTP_200_OK)
 
 
 class VerifyEmail(views.APIView):
